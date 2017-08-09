523a3f96c6d7ec2d7ace9bb424967808720b3e79
Md. Shaiful Islam <shaiful@codemate.com>
2017-08-09 17:53:14 +0600

    Configured JS and CSS for our App and setup Databases.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # laravel-blog
+
+## Installation
+
+#### Installation commands for NodeJS
+npm install buefy
+npm install font-awesome
+
+
---
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+
+class AdminLoginController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('guest:admins');
+    }
+    
+    public function showLoginForm()
+    {
+        return view('auth.admin-login');
+    }
+    
+    public function login(Request $request)
+    {
+        // Validate
+        $this->validate($request, [
+            'email' => 'required:email',
+            'password' => 'required:min:6',
+        ]);
+        
+        // Attempt to login
+        if(Auth::guard('admins')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember))
+        {
+            return redirect()->intended(route('admin.dashboard'));
+        }
+        
+        return redirect()->back()->withInput($request->only('email', 'remember'));
+    }    
+}
